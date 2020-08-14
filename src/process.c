d27c89855e2fc5972ee19b7abec0395dd2c9d177
giuliasteck <giuliasteck@gmail.com>
2020-08-13 21:36:52 -0300

    Bagunca

--- src/process.c
+++ src/process.c
@@ -1,32 +1,56 @@
 #include <imageprocessing.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/mman.h>
+#include <unistd.h>
 
-void blur(imagem *img, int i, int j);
+void *blur(float *arg, float* matriz, imagem*img);
 
 int main() {
-        imagem img;
+	imagem img;
         img = abrir_imagem("data/cachorro.jpg");
 
-        int i=0;
-        int j = 0;
-        blur(&img,i,j);
+	float *matriz1 = (float*)mmap(NULL, sizeof(float)*img.width*img.height, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON,0,0);
+	float *matriz2 = (float*)mmap(NULL, sizeof(float)*img.width*img.height, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON,0,0);
+	float *matriz3 = (float*)mmap(NULL, sizeof(float)*img.width*img.height, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANON,0,0);
 
-        salvar_imagem("cachorro-out.jpg", &img);
+        float* a1 = img.r;
+        float* a2 = img.g;
+        float* a3 = img.b;
+
+	pid_t p1, p2, p3;
+
+	p1= fork();
+	if (p1==0)
+		blur(a1,matriz1,&img);
+	p2 = fork();
+	if (p2==0)
+		blur(a2,matriz2,&img);
+	p3 = fork();
+	if (p3==0)
+		blur(a3, matriz3,&img);
+
+	waitpid(p1,NULL,0);
+	waitpid(p2,NULL,0);
+	waitpid(p3,NULL,0);
+
+        salvar_imagem("cachorro-out-process.jpg", &img);
         liberar_imagem(&img);
         return 0;
 }
 
 
-void blur(imagem *img, int i, int j){
+void *blur(float* arg, float *matriz, imagem*img){
         float alpha = 0.98;
-        for (i = 0; i<(img->width); i++){
-                for (j =0; j<(img->height); j++){
+        for (int i = 0; i<(img->width); i++){
+                for (int j =0; j<(img->height); j++){
                         if (i!=0) {
-                                img->g[j*img->width + i] = (1-alpha)*img->g[j*img->width + i] +(alpha)* img->g[j*img->width + i -1];
-                                img->b[j*img->width+i] = (1-alpha)*img->b[j*img->width + i]+(alpha)*img->b[j*img->width+i-1];
-                                img->r[j*img->width+i] = (1-alpha)*img->r[j*img->width+i]+(alpha)*img->r[j*img->width+i-1];
+                               arg[j*img->width + i] = (1-alpha)*arg[j*img->width + i] +(alpha)*arg[j*img->width + i -1];
                         }
-                }
+			matriz[j*img->width+i] = arg[j*img->width + i];
+
+		}
         }
--- INSERT --                                                                                                                        1,29          Top
-
-
+}
